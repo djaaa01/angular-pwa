6770711e674f1afdfa99d476131dad3149246308
Arpad Bacos <arpad.bacos@bmwtechworks.ro>
2024-07-25 14:48:43 +0300

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NotificationService } from './services/notification.service';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,15 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'angular-pwa';
+  constructor(private notificationService: NotificationService) {}
+
+  ngOnInit(): void {
+    this.notificationService.requestPermission();
+  }
+
+  notify(): void {
+    this.notificationService.showNotification('Notificare PWA', {
+      body: 'Bommmba !!!!!!',
+    });
+  }
 }
